e001d32c39ae3c54b657959036a6acf1d29c0df0
prashant <prashant.hariharan@externals.adidas.com>
2020-05-04 11:48:27 +0200

    Changes for elastic search integration on aws

--- elasticsearchnodb/src/main/java/com/prashant/elasticsearch/config/EsConfigHighLevelClient.java
+++ elasticsearchnodb/src/main/java/com/prashant/elasticsearch/config/EsConfigHighLevelClient.java
@@ -2,28 +2,32 @@ package com.prashant.elasticsearch.config;
 
 import org.apache.http.HttpHost;
 import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.data.elasticsearch.config.AbstractElasticsearchConfiguration;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 
 @Profile("!test")
 @Configuration
 @EnableElasticsearchRepositories(basePackages = "com.prashant.elasticsearch.es.repo")
-public class EsConfigHighLevelClient {
+public class EsConfigHighLevelClient extends AbstractElasticsearchConfiguration {
   @Value("${elasticsearch.host}")
   private String EsHost;
-
-  @Value("${elasticsearch.port}")
-  private int EsPort;
 
   @Value("${elasticsearch.clustername}")
   private String EsClusterName;
 
+  @Override
   @Bean
-  public RestClient restClient() {
-    return RestClient.builder(new HttpHost(EsHost, EsPort)).build();
+  public RestHighLevelClient elasticsearchClient() {
+
+    RestHighLevelClient client = new RestHighLevelClient(
+      RestClient.builder(HttpHost.create(EsHost)));
+
+    return client;
   }
 
 }
